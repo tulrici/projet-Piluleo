d9549b421e38fda681563ef13ebeee5ff7d8212c
Clément Stoyanov <c.stoyanov@it-students.fr>
2024-07-05 15:54:50 +0200

    Ajout d'un menu déroulant premiere version ainsi qu'une horloge

---
+++ Web front/js/menuDeroulant.js
@@ -0,0 +1,9 @@
+
+document.getElementById('menu_bouton').addEventListener('click', function() {
+    var dropdown = document.getElementById('menu_deroulant');
+    if (dropdown.style.display === 'none' || dropdown.style.display === '') {
+        dropdown.style.display = 'flex'; 
+    } else {
+        dropdown.style.display = 'none';
+    }
+});
